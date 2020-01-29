12bfa11a757a2c4e5c3f01a17b49b3512d0efe11
Lusgem <florian.lherbeil@yahoo.fr>
2020-01-29 17:03:31 +0100

    AJout clé privée

--- src/Chiffrement.java
+++ src/Chiffrement.java
@@ -34,6 +34,32 @@ public class Chiffrement {
 		return c;
 	}
 	
+	public static void privateKey() {
+		ArrayList<BigInteger> r = new ArrayList<>();
+		ArrayList<BigInteger> u = new ArrayList<>();
+		ArrayList<BigInteger> v = new ArrayList<>();
+		r.add(new BigInteger("7"));
+		r.add(new BigInteger("4992"));
+		u.add(new BigInteger("0"));
+		u.add(new BigInteger("1"));
+		v.add(new BigInteger("1"));
+		v.add(new BigInteger("0"));
+		while (r.get(r.size()-1).compareTo(new BigInteger("0"))!=0) {
+			BigInteger ri = r.get(r.size()-1);
+			BigInteger rim1 = r.get(r.size()-2);
+			BigInteger ui = u.get(r.size()-1);
+			BigInteger uim1 = u.get(r.size()-2);
+			BigInteger vi = v.get(r.size()-1);
+			BigInteger vim1 = v.get(r.size()-2);
+			r.add(rim1.subtract(rim1.divide(ri).multiply(ri)));
+			u.add(uim1.subtract(rim1.divide(ri).multiply(ui)));
+			v.add(vim1.subtract(rim1.divide(ri).multiply(vi)));
+		}
+		BigInteger ufinal = u.get(u.size()-2);
+		
+		
+	}
+	
 	public static void chiffrement(String message, Couple couple) {
 		ArrayList<Integer> ascii = new ArrayList<Integer>();
 		
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Chiffrement.publicKey(11);
+		Chiffrement.privateKey();
 		Chiffrement.chiffrement("bonjour", new Couple(new BigInteger("5141"), new BigInteger("7")));
 	}
 
