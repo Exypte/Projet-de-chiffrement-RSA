d05dcb9fc7495d233bdde5b7ed231629d3bf8cd0
Thibault <thib.condemine@outlook.com>
2020-01-29 15:39:09 +0100

    Clé public

---
+++ src/Couple.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+
+public class Couple {
+	private BigInteger n;
+	private BigInteger e;
+	
+	public Couple(BigInteger n, BigInteger e) {
+		super();
+		this.n = n;
+		this.e = e;
+	}
+	
+	public BigInteger getN() {
+		return n;
+	}
+	public void setN(BigInteger n) {
+		this.n = n;
+	}
+	public BigInteger getE() {
+		return e;
+	}
+	public void setE(BigInteger e) {
+		this.e = e;
+	}
+	
+	
+}
